51e0029c385da7bdad5c2c689b286c54ffd06128
Victor Barros <victorwbarros@gmail.com>
2022-09-26 17:06:02 -0300

    Fix: Run Eslint && Prettier

--- front-end/cypress/e2e/HomeRecommendation.cy.js
+++ front-end/cypress/e2e/HomeRecommendation.cy.js
@@ -1,25 +1,25 @@
+beforeEach(() => {
+  cy.resetDataBase();
+});
+describe("Create New Recommendation", () => {
+  it("Insert new Recommendation", () => {
+    cy.visit("http://localhost:3000/");
+    cy.intercept("POST", "/recommendations").as("PostRecommendation");
+    cy.get('input[placeholder="Name"]').type("video legal");
+    cy.get('input[placeholder="https://youtu.be/..."]').type(
+      "https://www.youtube.com/watch?v=wFBp_PHXrf4"
+    );
+    cy.get("div>button").click();
+    cy.wait("@PostRecommendation");
+    cy.get("article:first>div").contains("video legal").should("be.visible");
+  });
+});
 
-beforeEach(()=>{
-  cy.resetDataBase()
-})
-describe('Create New Recommendation', () => {
-  it('Insert new Recommendation', () => {
-    cy.visit('http://localhost:3000/');
-    cy.intercept('POST',"/recommendations").as("PostRecommendation")
-    cy.get('input[placeholder=\"Name"\]').type("video legal");
-    cy.get('input[placeholder=\"https://youtu.be/..."\]').type("https://www.youtube.com/watch?v=wFBp_PHXrf4")
-    cy.get('div>button').click()
-    cy.wait("@PostRecommendation")
-    cy.get("article:first>div").contains("video legal").should('be.visible')
-  })
+describe("Get timeline", () => {
+  it("Most display Timeline", () => {
+    cy.populeBank();
 
-})
-
-describe('Get timeline',()=>{
-it('Most display Timeline',()=>{  
-  
-  cy.populeBank()
-
-  cy.visit('http://localhost:3000/');
-  cy.get("div>article").siblings().should("be.visible")})
-})
+    cy.visit("http://localhost:3000/");
+    cy.get("div>article").siblings().should("be.visible");
+  });
+});
--- front-end/cypress/e2e/voteRecommendation.cy.js
+++ front-end/cypress/e2e/voteRecommendation.cy.js
@@ -1,40 +1,40 @@
-beforeEach(()=>{
-  cy.resetDataBase()
-})
+beforeEach(() => {
+  cy.resetDataBase();
+});
 
-describe("Up Vote Recommendation",()=>{
-  it("Most Upvote Last Recommendation",()=>{
-    cy.visit('http://localhost:3000/');
-    cy.createRecomendation()
-    cy.get('article:first>div:last').invoke('text').should("eq","0")
-    cy.intercept("POST","recommendations/1/upvote").as("UpVote")
-    cy.get('article:first>div:last>svg:first').click()
-    cy.wait("@UpVote")
-    cy.get('article:first>div:last').invoke('text').should("eq","1")
-  })
-})
-describe("DownVote Recommendation",()=>{
-  it("Most Downvote Last Recommendation",()=>{
-    cy.visit('http://localhost:3000/');
-    cy.createRecomendation()
-    cy.get('article:first>div:last').invoke('text').should("eq","0")
-    cy.intercept("POST","recommendations/1/downvote").as("DownVote")
-    cy.get('article:first>div:last>svg:last').click()
-    cy.wait("@DownVote")
-    cy.get('article:first>div:last').invoke('text').should("eq","-1")
+describe("Up Vote Recommendation", () => {
+  it("Most Upvote Last Recommendation", () => {
+    cy.visit("http://localhost:3000/");
+    cy.createRecomendation();
+    cy.get("article:first>div:last").invoke("text").should("eq", "0");
+    cy.intercept("POST", "recommendations/1/upvote").as("UpVote");
+    cy.get("article:first>div:last>svg:first").click();
+    cy.wait("@UpVote");
+    cy.get("article:first>div:last").invoke("text").should("eq", "1");
   });
-  it("Most Downvote Recommendation 6 times and delete recommendation",()=>{
-    cy.visit('http://localhost:3000/');
-    cy.createRecomendation()
-    cy.get('article:first>div:last').invoke('text').should("eq","0")
-    cy.intercept("POST","recommendations/1/downvote").as("DownVote")
-    let i =1
-    while(i<=6){
-      cy.get('article:first>div:last>svg:last').click()
-      i++
+});
+describe("DownVote Recommendation", () => {
+  it("Most Downvote Last Recommendation", () => {
+    cy.visit("http://localhost:3000/");
+    cy.createRecomendation();
+    cy.get("article:first>div:last").invoke("text").should("eq", "0");
+    cy.intercept("POST", "recommendations/1/downvote").as("DownVote");
+    cy.get("article:first>div:last>svg:last").click();
+    cy.wait("@DownVote");
+    cy.get("article:first>div:last").invoke("text").should("eq", "-1");
+  });
+  it("Most Downvote Recommendation 6 times and delete recommendation", () => {
+    cy.visit("http://localhost:3000/");
+    cy.createRecomendation();
+    cy.get("article:first>div:last").invoke("text").should("eq", "0");
+    cy.intercept("POST", "recommendations/1/downvote").as("DownVote");
+    let i = 1;
+    while (i <= 6) {
+      cy.get("article:first>div:last>svg:last").click();
+      i++;
     }
-    cy.wait("@DownVote")
+    cy.wait("@DownVote");
 
-    cy.get('article:first').should("not.exist")
-  })
-})
+    cy.get("article:first").should("not.exist");
+  });
+});
